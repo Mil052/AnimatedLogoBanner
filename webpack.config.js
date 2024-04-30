271eb129d66ef856fd2124139d124bc96371854a
MiI0sz <gajda.milosz@gmail.com>
2024-04-30 11:41:12 +0200

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  performance: {
+    hints: 'warning',
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000,
+},
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './build',
+    watchFiles: ['src/**/*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: 'asset/inline',
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new CopyPlugin({patterns: [{ from: "src/assets", to: "assets" }]})
+  ],
+};
